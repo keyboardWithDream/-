ae88b4fc4021dadb21a7452508a8615778a95aac
keyboardWithDream <isharlan.hu@gmail.com>
2020-07-26 13:43:59 +0800

    Redis缓存优化

--- src/main/java/cn/study/travel/service/impl/CategoryServiceImpl.java
+++ src/main/java/cn/study/travel/service/impl/CategoryServiceImpl.java
@@ -4,8 +4,12 @@ import cn.study.travel.dao.CategoryDao;
 import cn.study.travel.dao.impl.CategoryDaoImpl;
 import cn.study.travel.domain.Category;
 import cn.study.travel.service.CategoryService;
+import cn.study.travel.util.JedisUtil;
+import redis.clients.jedis.Jedis;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Harlan
@@ -17,6 +21,22 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> findAll() {
-       return dao.findAll();
+        Jedis jedis = JedisUtil.getJedis();
+        Set<String> categorys = jedis.zrange("category", 0, -1);
+        List<Category> categoryList = null;
+        if (categorys == null || categorys.size() == 0){
+            categoryList = dao.findAll();
+            for (Category cs : categoryList) {
+                jedis.zadd("category", cs.getCid(), cs.getCname());
+            }
+        }else {
+            categoryList = new ArrayList<>();
+            for (String name : categorys) {
+                Category c = new Category();
+                c.setCname(name);
+                categoryList.add(c);
+            }
+        }
+        return categoryList;
     }
 }
